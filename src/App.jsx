61013213b6a218db0e052c7577bbb0c5c78c1a3a
IK8933 <iankessack1989@gmail.com>
2025-01-14 21:10:59 -0500

    better foundation

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+// import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+
+//dictionaryselector
+
+//wordsearch 
+
+
+export default App
+// export const wordsUrl = `${dictionaryUrl}/only-words`;
+// export const dictionariesUrl = `${dictionaryUrl}/multi-details`;
